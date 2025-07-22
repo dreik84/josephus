03ec8e7ca3c1e28c31f32d6f862c1cf517ebe0d5
dreik84 <dreik84@mail.ru>
2025-07-22 16:44:01 +0300

    Add App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+class App {
+    public static void main(String[] args) {
+
+        CyclicLinkedList list = new CyclicLinkedList();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        System.out.println(list);
+    }
+}
