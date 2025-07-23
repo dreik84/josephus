dcc32555add0697379f02d55357f3960dc4a624c
dreik84 <dreik84@mail.ru>
2025-07-23 16:27:20 +0300

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -7,10 +7,15 @@ class App {
 
         list.add(1);
         list.add(2);
+        list.add(2);
+        list.add(2);
         list.add(3);
         list.add(4);
         list.add(5);
 
+        list.delete(2);
+
+        System.out.println(list.find(3));
         System.out.println(list);
     }
 }
