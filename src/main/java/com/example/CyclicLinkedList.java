6165f9f502b52915a402381f4b0dab38c4d5e72f
dreik84 <dreik84@mail.ru>
2025-07-23 16:27:36 +0300

    Update CyclicLinkedList.java

--- src/main/java/com/example/CyclicLinkedList.java
+++ src/main/java/com/example/CyclicLinkedList.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.StringJoiner;
+
 public class CyclicLinkedList {
 
     private Link current;
@@ -7,47 +9,72 @@ public class CyclicLinkedList {
     public void add(int value) {
         if (current == null) {
             current = new Link(value);
-        } else if (current.next == null){
+        } else if (current.next == null) {
             current.next = new Link(value);
             current.next.next = current;
+            current = current.next;
         } else {
             Link newLink = new Link(value);
             newLink.next = current.next;
             current.next = newLink;
+            current = current.next;
         }
     }
 
-    public void search(int value) {
-
-    }
-
-    public void delete(int value) {
-
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
+    public Link find(int value) {
         Link end = current;
 
         do {
-            sb.append(current.value);
+            if (current.value == value) return current;
             current = current.next;
 
         } while (current != end);
 
-        return sb.toString();
+        return null;
+    }
+
+    public void delete(int value) {
+        Link previous = current;
+        Link end = current.next;
+        current = current.next;
+
+        do {
+            if (current.value == value) {
+                previous.next = current.next;
+                current = previous.next;
+            } else {
+                previous = current;
+                current = current.next;
+            }
+
+        } while (current != end);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner sj = new StringJoiner(", ");
+        Link end = current;
+
+        do {
+            sj.add(String.valueOf(current.value));
+            current = current.next;
+
+        } while (current != end);
+
+        return "CyclicLinkedList { " + sj + " }";
     }
 
     private static class Link {
         int value;
         Link next;
 
-        Link() {
-        }
-
         Link(int value) {
             this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Link { " + "value=" + value + " }";
         }
     }
 }
